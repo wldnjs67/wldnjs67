ccdb542e9443990cb8008952c5955f13d701fffd
wldnjs67 <jw020607@gmail.com>
2024-09-14 22:09:12 +0900

    week2

--- week2.c
+++ week2.c
@@ -4,14 +4,6 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a,b; // 변수 선언 
-	
 	printf("Hello, Sookmyung Electronics!\n");
-	
-	a=10; //변수 정의 
-	b=20; //변수 정의 
-	
-	printf("%d + %d = %d\n", a, b, a+b); //수식과 결과값 계산 출력 
-	
 	return 0;
 }
