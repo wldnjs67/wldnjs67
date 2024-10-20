84aa142aaec1904906f3537df9bd953f07683333
wldnjs67 <jw020607@gmail.com>
2024-10-20 12:12:37 +0900

    week7

--- week07/main7.2.c
+++ week07/main7.2.c
@@ -3,43 +3,33 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-	int sumTwo (int a, int b)
-	{
-		int sum;
-		sum = a+b;
-		return sum;
-	}
-	
-	int square (int n)
-	{
-		int square;
-		square = n*n;
-		return square;
-	}
-	
-	int get_max (int x, int y)
-	{
-		int max;
-		if (x>y)
-		{
-			max=x;
-		}
-		else 
-		{
-			max=y;
-		}
-	}
-	
-	int main()
-	{
-		int num1, num2, sum, square, max;
-		printf("Enter the integers: ");
-		scanf("%d %d", &num1, &num2);
-		
-		sum = int sumTwo (num1, num2);
-		square = int square (num1);
-		max = int get_max (num1, num2);
-		
-		printf("Result - Sum: %d, Square: %d, Max: %d", sum, square, max);
-	}
+#include <stdio.h>  // Required for printf and scanf
+
+int sumTwo(int a, int b) {
+    return (a + b);
 }
+
+int square(int n) {
+    return (n * n);
+}
+
+int get_max(int x, int y) {
+    if (x > y) return x;
+    else return y;
+}
+
+int main() {
+    int num1, num2, sum, sq, max;
+
+    printf("Enter the integers: ");
+    scanf("%d %d", &num1, &num2);
+
+    sum = sumTwo(num1, num2);
+    sq = square(num1);
+    max = get_max(num1, num2);
+
+    printf("Result - Sum: %d, Square: %d, Max: %d\n", sum, sq, max);
+
+    return 0;
+}
+
